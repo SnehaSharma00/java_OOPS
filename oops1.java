92ae0a2d4abfe8a690e085c06e7210da4e014588
SnehaSharma00 <sanuwkuku@gmail.com>
2026-09-24 23:51:49 +0530

    code

--- oops1.java
+++ oops1.java
@@ -1,3 +1,37 @@
 public class oops1 {
+    private String name;
+    private int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+   
     
+
+class Student extends Person {
+    private int rollNo;
+
+    Student(String name, int age, int rollNo) {
+        super(name, age);
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Roll No: " + rollNo);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Sneha", 22, 101);
+
+        s1.display();
+
+        System.out.println(s1.getName());
+    }
 }
